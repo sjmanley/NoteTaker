de7e97f42e9aa864ce8df93ea19263250ff01606
Scott Manley <scottmanley@SJM-MacBook-Pro.local>
2020-02-29 13:02:35 -0600

    saving my notes

--- server.js
+++ server.js
@@ -4,73 +4,73 @@ const fs = require("fs");
 
 //sets up express app
 const app = express();
-const dbFile = "db/db.json";
+const dbFile = "/db.json";
 
 app.use(express.static("assets"));
 const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Routes
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
-app.get("notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/notes.html"));
 });
-app.get("api/notes", (req, res) => {
-    // Return the DB file
-    res.sendFile(path.join(__dirname, "db/db.json"));
-  });
-app.post("api/notes", (req, res) => {
-    const data = req.body;
-    const id = Date.now();
-    const db = fs.readFileSync(path.join(__dirname, "db/db.json"), "utf8");
-    const note = {
-      id: id,
-      title: data.title,
-      text: data.text,
-    };
-    const dbObj = JSON.parse(db);
-    dbObj.push(note);
-    fs.writeFileSync(path.join(__dirname, "db/db.json"), JSON.stringify(dbObj), "utf8");
-    res.sendFile(path.join(__dirname, "db/db.json"));
-  });
-  
-  app.delete("api/notes/:id", (req, res) => {
+app.get("/api/notes", (req, res) => {
+  // Return the DB file
+  res.sendFile(path.join(__dirname, "/db.json"));
+});
+app.post("/api/notes", (req, res) => {
+  const data = req.body;
+  const id = Date.now();
+  const db = fs.readFileSync(path.join(__dirname, "/db.json"), "utf8");
+  const note = {
+    id: id,
+    title: data.title,
+    text: data.text,
+  };
+  const dbInfo = JSON.parse(db);
+  dbInfo.push(note);
+  fs.writeFileSync(path.join(__dirname, "/db.json"), JSON.stringify(dbInfo), "utf8");
+  res.sendFile(path.join(__dirname, "/db.json"));
+});
 
-    const id = req.params.id;
+app.delete("/api/notes/:id", (req, res) => {
 
-    const db = fs.readFileSync(path.join(__dirname, "db/db.json"), "utf8");
+  const id = req.params.id;
 
-    const dbObj = JSON.parse(db);
-    for (let x = 0; x < dbObj.length; x++) {
-      if (dbObj[x].id.toString() === id) {
-        dbObj.splice(x, 1);
-        break;
-      }
+  const db = fs.readFileSync(path.join(__dirname, "/db.json"), "utf8");
+
+  const dbInfo = JSON.parse(db);
+  for (let x = 0; x < dbInfo.length; x++) {
+    if (dbInfo[x].id.toString() === id) {
+      dbInfo.splice(x, 1);
+      break;
     }
+  }
 
-    fs.writeFileSync(path.join(__dirname, "db/db.json"), JSON.stringify(dbObj), "utf8");
-    // Return the DB file
-    res.sendFile(path.join(__dirname, "db/db.json"));
-  });
+  fs.writeFileSync(path.join(__dirname, "/db.json"), JSON.stringify(dbInfo), "utf8");
+  // Return the DB file
+  res.sendFile(path.join(__dirname, "/db.json"));
+});
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
 });
 
 function getNewId() {
-    let newId = {};
-    console.log(dbNotes);
-    if (typeof dbNotes.id === "undefined") {
-      newId.id = 0;
-    }
-    else {
-      console.log (dbNotes[0].id);
-      newId.id = dbNotes[dbNotes.length -1].id + 1; //gets last ID 
-    }
-    debug && console.log(`lastID of getNewId: ${newId}`);
-    return newId;
+  let newId = {};
+  console.log(dbNotes);
+  if (typeof dbNotes.id === "undefined") {
+    newId.id = 0;
   }
+  else {
+    console.log(dbNotes[0].id);
+    newId.id = dbNotes[dbNotes.length - 1].id + 1; //gets last ID 
+  }
+  debug && console.log(`lastID of getNewId: ${newId}`);
+  return newId;
+}
